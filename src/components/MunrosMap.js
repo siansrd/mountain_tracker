79c0e591a4b1631dd97b7bc4a124873c3060cb43
siansrd <sianrobinsondavies@gmail.com>
2018-11-13 10:52:18 +0000

    Render maps of scotland

--- src/components/MunrosMap.js
+++ src/components/MunrosMap.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+import './MunrosMap.css';
 
 class MunrosMap extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      lat: 57.4,
+      lng: -4.6,
+      zoom: 7,
+    }
+  }
+
   render() {
+    const position = [this.state.lat, this.state.lng]
     return (
-      <h2>Map</h2>
+      <Map center={position} zoom={this.state.zoom}>
+        <TileLayer
+          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+        />
+      </Map>
     );
   }
 
