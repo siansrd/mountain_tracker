4b43766f835d609b401c677cbdf7a2a065821b18
siansrd <sianrobinsondavies@gmail.com>
2018-11-13 09:36:04 +0000

    Add and render container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import MunrosContainer from './containers/MunrosContainer.js'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <MunrosContainer />
     );
   }
 }
---
+++ src/containers/MunrosContainer.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+class MunrosContainer extends Component {
+
+  render() {
+    return (
+      <>
+        <h1>Hello World</h1>
+      </>
+    )
+  }
+
+}
+
+export default MunrosContainer
