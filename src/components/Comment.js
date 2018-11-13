979d7c6b74885b70b0b765928e6bf5c73fa439e3
siansrd <sianrobinsondavies@gmail.com>
2018-11-13 09:46:20 +0000

    Add and render comment component

---
+++ src/components/Comment.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Comment = (props) => {
+  return (
+    <h2>Comment</h2>
+  )
+}
+
+export default Comment
--- src/components/MunroDetailBox.js
+++ src/components/MunroDetailBox.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import MunroDetails from './MunroDetails'
+import Comment from './Comment'
 
 const MunroDetailBox = (props) => {
   return (
-    <MunroDetails />
+    <>
+      <MunroDetails />
+      <Comment />
+    </>
   )
 }
 
